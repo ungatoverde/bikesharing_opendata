09dbbe49ea1e47029a2ab924fdd26416f85b10a9
ungatoverde <23521196+ungatoverde@users.noreply.github.com>
2021-12-27 15:09:38 +0100

    Defining drop-down list

--- app.R
+++ app.R
@@ -1,4 +1,27 @@
 # Load packages
 library(shiny)
 
-# Test changes
+# Define UI
+ui <- fluidPage(
+  # App title
+  titlePanel("Bike-sharing Open Data"),
+  
+  # Sidebar layout with input and output definitions ----
+  sidebarLayout(
+    
+    # Sidebar panel for inputs ----
+    sidebarPanel(
+      
+      # Input: Slider for the number of bins ----
+      selectInput(inputId = "city",
+                  label = "Nombre de la ciudad / Name of the city",
+                  choices = list("Madrid" = 1, "Zaragoza" = 2, "Elche" = 3))
+      
+    ),
+    
+    # Main panel for displaying outputs ----
+    mainPanel(
+      
+    )
+  )
+)
